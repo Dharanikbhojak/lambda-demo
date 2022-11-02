24cbc3dc4ad63a1395aa1b428888d12ca6e7cf9a
Dharanikbhojak <116163544+Dharanikbhojak@users.noreply.github.com>
2022-11-02 18:12:55 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function handler(event) {
-    console.log("Job Stared" )
-
-    return {
-        body: JSON.stringify({message: 'SUCCESS'}),
-        statusCode: 200,
-    };
-}
-module.exports = {handler};
